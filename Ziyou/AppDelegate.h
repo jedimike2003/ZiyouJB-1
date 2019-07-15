0784a29d0c68810e803fc46ea68dc5db7d98a79d
jay <38664452+sukarodo@users.noreply.github.com>
2019-07-15 12:01:41 -0500

    Update AppDelegate.h

--- Ziyou/AppDelegate.h
+++ Ziyou/AppDelegate.h
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.h
-//  Slice
+//  Ziyou
 //
 //  Created by Tanay Findley on 5/7/19.
-//  Copyright © 2019 Slice Team. All rights reserved.
+//  Copyright © 2019 Ziyou Team. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
