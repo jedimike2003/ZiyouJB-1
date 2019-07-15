debf95f8b3c22a2cbb33a86392aff453c274a13d
jay <38664452+sukarodo@users.noreply.github.com>
2019-07-15 12:08:51 -0500

    i am ziyou

--- Ziyou/lib/loadshiz/bypass.m
+++ Ziyou/lib/loadshiz/bypass.m
@@ -1,9 +1,9 @@
 //
 //  bypass.c
-//  Slice
+//  Ziyou
 //
 //  Created by Tanay Findley on 5/19/19.
-//  Copyright © 2019 Slice Team. All rights reserved.
+//  Copyright © 2019 Ziyou Team. All rights reserved.
 //
 
 #include "bypass.h"
